69cae17871eec5f8befc0dfe4193168f44e5a831
Norbert Csibi <cibi_norbert@yahoo.com>
2021-06-15 11:08:25 +0100

    fixed the missile and added explosion effect

--- Tower-Defense-v0.1/Assets/Bullet.cs
+++ Tower-Defense-v0.1/Assets/Bullet.cs
@@ -9,6 +9,7 @@ public class Bullet : MonoBehaviour
     public Transform target;
     public float speed = 55f;
     public GameObject impactEffect;
+    public float explosionRadius = 0f;
 
     // Update is called once per frame
     void Update()
@@ -30,23 +31,43 @@ public class Bullet : MonoBehaviour
             return;
         }
 
-        // normilized means constant speed
+        // normilized means constant speed ( move missle toward enemy)
         transform.Translate(direction.normalized * distanceThisFrame, Space.World);
+        //Rotate bullet towards enemy pos
+        transform.LookAt(target);
+
     }
 
     private void HitTarget()
     {
         Destroy(gameObject);
 
-        // Destroy enemy
-        Destroy(target.gameObject);
+        if (explosionRadius > 0)
+        {
+            Explode();
+        } else
+        {
+            Damage(target);
+        }
 
         // Show effect once hit
         GameObject effectInstance = (GameObject)Instantiate(impactEffect, transform.position, transform.rotation);
-        // Destroy after 2 seconds
-        Destroy(effectInstance, 2f);
+        // Destroy after 5 seconds
+        Destroy(effectInstance, 5f);
+    }
 
-
+    private void Explode()
+    {
+        // Shoots a sphere and check all of the colliders that overlap with the sphere. Gets all of them
+        Collider[] allEnemyInRange =  Physics.OverlapSphere(transform.position, explosionRadius);
+        foreach (Collider enemyInRange in allEnemyInRange)
+        {
+            // Check if the object has enemy tag
+            if (enemyInRange.tag == "Enemy")
+            {
+                Damage(enemyInRange.transform);
+            }
+        }
     }
 
     // This method is called in the turret to setup the enemy
@@ -54,5 +75,17 @@ public class Bullet : MonoBehaviour
     {
         // Set enemy
         target = inputTarget;
+    }
+
+    void Damage(Transform enemy)
+    {
+        // Destroy enemy
+        Destroy(enemy.gameObject);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, explosionRadius);
     }
 }
