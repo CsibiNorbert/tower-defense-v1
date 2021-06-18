9d3bca9b50177c3d26a1e1e0a84b2de4e1736055
Norbert Csibi <cibi_norbert@yahoo.com>
2021-06-18 22:18:26 +0100

    pause menu & effects

---
+++ Tower-Defense-v0.1/Assets/PauseScript.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseScript : MonoBehaviour
+{
+    public GameObject pauseUi;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            TogglePauseMenu();
+        }
+    }
+
+    public void TogglePauseMenu()
+    {
+        // We invert the state, true false etc by getting the current state of the paused ui
+        pauseUi.SetActive(!pauseUi.activeSelf);
+
+        // Freeze time
+        if (pauseUi.activeSelf)
+        {
+            // Timescale is the speed at which the game is running.
+            Time.timeScale = 0f;
+        } else
+        {
+            Time.timeScale = 1f;
+        }
+    }
+
+    public void Retry()
+    {
+        TogglePauseMenu();
+        // Return active scene.
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void Menu()
+    {
+
+    }
+
+
+}
